89fe6feb934fbe84d4e46bf3c20ae6a13e3babd7
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-03-24 23:29:47 -0600

    ➕ implement visual mode hook

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
 import "./styles.scss";
 import Empty from "./Empty.jsx";
 import Show from "./Show.jsx";
 import Header from "./Header.jsx";
+import useVisualMode from "../../hooks/useVisualMode";
 
 export default function Appointment(props) {
-
-  return (<article className="appointment">
-
-    <>
-      <Header time={props.time} />
-      {props.interview ? <Show stuent={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
-    </>
-  </article>);
-
+  return (
+    <article className='appointment'>
+      <>
+        <Header time={props.time} />
+        {props.interview ? (
+          <Show
+            stuent={props.interview.student}
+            interviewer={props.interview.interviewer}
+          />
+        ) : (
+          <Empty />
+        )}
+      </>
+    </article>
+  );
 }
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export default function useVisualMode(initial) {
+
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (replace) {
+      history.pop();
+    }
+    setMode(newMode);
+    setHistory([...history, newMode]);
+  }
+
+  function back() {
+    if (history.length === 1) {
+      return;
+    }
+    history.pop();
+    setMode(history[history.length - 1]);
+  }
+
+  return { mode, transition, back };
+
+
+}
