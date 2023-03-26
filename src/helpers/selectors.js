8e8a370398dc73d36a82d9195667dd792b831aab
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-03-26 00:00:59 -0600

    🛠 fix linting issues

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,49 +1,47 @@
 export function getAppointmentsForDay(state, day) {
-
-
-  const filteredDay = state.days.find(e => e.name === day);
+  const filteredDay = state.days.find((e) => e.name === day);
   if (!filteredDay) {
     return [];
   }
   const appointments = filteredDay.appointments;
 
   if (!appointments) {
-    return []
+    return [];
   }
 
-  const appointmentsForDay = Object.values(state.appointments).filter(appointment => {
-    return appointments.includes(appointment.id)
-  });
+  const appointmentsForDay = Object.values(state.appointments).filter(
+    (appointment) => {
+      return appointments.includes(appointment.id);
+    }
+  );
   return appointmentsForDay;
-
 }
 
-
 export function getInterview(state, interview) {
-
   if (!interview) {
     return null;
   }
-  const interviewer = Object.values(state.interviewers).find(i => i.id === interview.interviewer);
-  const interviewData = { ...interview, interviewer }
+  const interviewer = Object.values(state.interviewers).find(
+    (i) => i.id === interview.interviewer
+  );
+  const interviewData = { ...interview, interviewer };
   return interviewData;
-
 }
 
 export function getInterviewersForDay(state, day) {
-
-  const filteredDay = state.days.find(e => e.name === day);
+  const filteredDay = state.days.find((e) => e.name === day);
   if (!filteredDay) {
     return [];
   }
   const interviewersIdsForDay = filteredDay.interviewers;
 
   if (!interviewersIdsForDay) {
-    return []
+    return [];
   }
-  const interviewersForDay = Object.values(state.interviewers).filter(interviewer => {
-    return interviewersIdsForDay.includes(interviewer.id)
-  });
+  const interviewersForDay = Object.values(state.interviewers).filter(
+    (interviewer) => {
+      return interviewersIdsForDay.includes(interviewer.id);
+    }
+  );
   return interviewersForDay;
-
 }
