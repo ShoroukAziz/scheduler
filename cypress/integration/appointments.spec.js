a1ca6edc72dc0d40e19d967e60eb6f65defa20f2
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-03-29 19:48:10 -0600

    🛠 unskip all tests

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -7,7 +7,7 @@ describe('Appointments', () => {
     cy.contains('Monday');
   });
 
-  xit('should book an interview', () => {
+  it('should book an interview', () => {
     cy.get('[alt=Add]').first().click();
     cy.get('[data-testid=student-name-input]').type('Lydia Miller-Jones');
     cy.get("[alt='Sylvia Palmer']").click();
@@ -16,7 +16,7 @@ describe('Appointments', () => {
     cy.contains('.appointment__card--show', 'Sylvia Palmer');
   });
 
-  xit('should edit an interview', () => {
+  it('should edit an interview', () => {
     cy.get("[alt='Edit']").invoke('show').click();
     cy.get('[data-testid=student-name-input]').clear().type('Sara Carpenter');
     cy.get("[alt='Tori Malcolm']").click();
