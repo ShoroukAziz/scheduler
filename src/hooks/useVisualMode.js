c878b224d34f2e585223b68be50f2e664cbb40ba
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-03-30 21:28:50 -0600

    ➕ add comments

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
+  /**
+   * Transitions forward to a new mode of the appointment component
+   *
+   * @param {string}  newMode  the mode that we want to transition to
+   * @param {boolean} replace  a boolean used to skip going back to some modes like confirmation mode
+   */
   function transition(newMode, replace = false) {
     setMode(newMode);
     if (replace) {
@@ -17,6 +23,9 @@ export default function useVisualMode(initial) {
     setHistory((prevHistory) => [...prevHistory, newMode]);
   }
 
+  /**
+   * Transitions backwards to the previous appointment mode
+   * */
   function back() {
     if (history.length === 1) {
       return;
