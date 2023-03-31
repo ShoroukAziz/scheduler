b938f139478a9c715f0b787f500276b177438ffd
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-03-30 21:12:08 -0600

    ➖ remove redundant  callbacks

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import "./styles.scss";
-import Empty from "./Empty.jsx";
-import Confirm from "./Confirm.jsx";
-import Show from "./Show.jsx";
-import Header from "./Header.jsx";
-import Form from "./Form";
-import useVisualMode from "../../hooks/useVisualMode";
-import Status from "./Status";
-import Error from "./Error";
+import React from 'react';
+import './styles.scss';
+import Empty from './Empty.jsx';
+import Confirm from './Confirm.jsx';
+import Show from './Show.jsx';
+import Header from './Header.jsx';
+import Form from './Form';
+import useVisualMode from '../../hooks/useVisualMode';
+import Status from './Status';
+import Error from './Error';
 
 export default function Appointment(props) {
-  const EMPTY = "EMPTY";
-  const SHOW = "SHOW";
-  const CREATE = "CREATE";
-  const SAVING = "SAVING";
-  const DELETING = "DELETING";
-  const EDIT = "EDIT";
-  const CONFIRM = "CONFIRM";
-  const ERROR_SAVE = "ERROR_SAVE";
-  const ERROR_DELETE = "ERROR_DELETE";
+  const EMPTY = 'EMPTY';
+  const SHOW = 'SHOW';
+  const CREATE = 'CREATE';
+  const SAVING = 'SAVING';
+  const DELETING = 'DELETING';
+  const EDIT = 'EDIT';
+  const CONFIRM = 'CONFIRM';
+  const ERROR_SAVE = 'ERROR_SAVE';
+  const ERROR_DELETE = 'ERROR_DELETE';
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -53,18 +53,16 @@ export default function Appointment(props) {
   }
 
   return (
-    <article className="appointment" data-testid="appointment">
+    <article className='appointment' data-testid='appointment'>
       <>
         <Header time={props.time} />
         {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-        {mode === SAVING && <Status message="Saving..." />}
-        {mode === DELETING && <Status message="Deleting..." />}
+        {mode === SAVING && <Status message='Saving...' />}
+        {mode === DELETING && <Status message='Deleting...' />}
         {mode === CONFIRM && (
           <Confirm
-            message="Are you sure you would like to delete?"
-            onCancel={() => {
-              back();
-            }}
+            message='Are you sure you would like to delete?'
+            onCancel={back}
             onConfirm={cancel}
           />
         )}
@@ -84,9 +82,7 @@ export default function Appointment(props) {
           <Form
             interviewers={props.interviewers}
             onSave={save}
-            onCancel={() => {
-              back();
-            }}
+            onCancel={back}
           />
         )}
         {mode === EDIT && (
@@ -95,27 +91,15 @@ export default function Appointment(props) {
             student={props.interview.student}
             interviewer={props.interview.interviewer.id}
             onSave={save}
-            onCancel={() => {
-              back();
-            }}
+            onCancel={back}
             update={true}
           />
         )}
         {mode === ERROR_SAVE && (
-          <Error
-            message="Could not save appointment"
-            onClose={() => {
-              back();
-            }}
-          />
+          <Error message='Could not save appointment' onClose={back} />
         )}
         {mode === ERROR_DELETE && (
-          <Error
-            message="Could not cancel appointment"
-            onClose={() => {
-              back();
-            }}
-          />
+          <Error message='Could not cancel appointment' onClose={back} />
         )}
       </>
     </article>
